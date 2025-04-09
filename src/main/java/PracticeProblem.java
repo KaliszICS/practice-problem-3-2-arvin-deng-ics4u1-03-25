c5acc2495a516d7d3637cbd52e7514816f4b791e
arvin-deng-ics4u1-03-25 <arvin.deng@student.tdsb.on.ca>
2025-04-08 22:25:47 -0400

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,63 @@
+import java.util.Arrays;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
+	public static int towerOfHanoi(int num) {
+        if (num < 3) {
+            return -1;
+        }
+        return hanoiRecursive(num);
+    }
 
-	public static void q2() {
-		//Write question 2 code here
-	}
+    public static int hanoiRecursive(int num) {
+        if (num == 1) {
+            return 1;
+        }
+        return 2 * hanoiRecursive(num - 1) + 1;
+    }
 
-	public static void q3() {
-		//Write question 3 code here
-	}
+	public static int[]compareStringSearch(String[] array, String text) {
+    	int loops1 = 0;
+   		int loops2 = 0;
+    	Arrays.sort(array);
 
-	public static void q4() {
-		//Write question 4 code here
-	}
+    for (int i = 0;i < array.length;i++) {
+        array[i].toLowerCase();
+    }
 
-	public static void q5() {
-		//Write question 5 code here
-	}
+    for (int i = 0;i < array.length;i++) {
+        loops1++;
+    if (array[i].equals(text)) {
+        break;
+    }
+}
+    int low = 0;
+    int high = array.length - 1;
+    int mid = (low + high)/2;
+
+    while (low <= high) {
+    loops2++;
+        int comparison = array[mid].compareTo(text);
+        if (comparison < 0) {
+            low = mid + 1;
+            mid = (high + low) / 2;
+        }
+        else if (comparison > 0) {
+            high = mid - 1;
+            mid = (high + low) / 2;
+        }
+        else {
+            low = high + 1;
+        }
+    }
+
+	int [] stringArr = {loops1, loops2};
+
+	return stringArr;
+}
 
 }
